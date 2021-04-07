6122fa95ca47c3dd4acf4abc8e18195e17773668
uf0 <scnepa222@gmail.com>
2021-04-07 16:07:01 +0200

    added settings

--- src/FileSaver.js
+++ src/FileSaver.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
-import DropZone from "./DropZone"
-import Icon from "./Icon"
+import DropZone from './DropZone'
+import Icon from './Icon'
 import { saveAs } from 'file-saver'
 import * as d3 from 'd3'
 
 const FileSaver = () => {
 	const [files, setFiles] = useState([])
+	const [proxy, setProxy] = useState('')
+	const [throttle, setThrottle] = useState(50)
 
 	const loadFile = (input) => {
 		const file = input.files[0]
@@ -15,32 +17,64 @@ const FileSaver = () => {
 
 		const reader = new FileReader()
 		reader.onload = function (event) {
-			setFiles(dsv.parse(event.target.result, (d) => ({ status: "queued", ...d })))
+			setFiles(dsv.parse(event.target.result, (d) => ({ status: 'queued', ...d })))
 		}
 		reader.readAsText(file)
 	}
-	const downloadFiles = () => {
-    console.log(files)
-    files.forEach((f, i) => {
-      f.status="progress"
-      setFiles([...files])
-			saveAs('https://api.allorigins.win/raw?url=' + f.url, f.fileName)
-		})
-  }
-  
-  
+	const downloadFiles = async () => {
+		for (const file of files) {
+			file.status = 'progress'
+			setFiles([...files])
+			saveAs(proxy + file.url, file.fileName)
+			await sleep(throttle)
+		}
+
+		function sleep(ms) {
+			return new Promise((resolve) => setTimeout(resolve, ms))
+		}
+	}
+
 	return (
 		<>
 			{!files.length && <input type="file" accept=".tsv, .csv" onChange={(event) => loadFile(event.target)} />}
-      {!files.length===-2 && <DropZone />}
-			{files.length && (
+			{!files.length === -2 && <DropZone />}
+			<div className="mt-3">
+				<h6>Settings</h6>
+				<form>
+					<div className="form-row">
+						<div className="form-group col-md-6">
+							<label for="throttle">throttle (millisec): </label>
+							<input
+								type="number"
+								value={throttle}
+								onChange={(event) => setThrottle(event.target.value)}
+								type="number"
+								min="0"
+								step="1"
+								id="throttle"
+								className="form-control"
+							/>
+						</div>
+					</div>
+					<div className="form-row">
+						<div className="form-group col-md-6">
+							<label for="proxy">
+								proxy url (eg: <kbd>https://api.allorigins.win/raw?url=</kbd>){' '}
+							</label>
+							<input type="text" value={proxy} onChange={(event) => setProxy(event.target.value)} id="proxy" className="form-control" />
+						</div>
+					</div>
+				</form>
+			</div>
+
+			{files.length > 0 && (
 				<>
 					<div>
 						{files.map((f, i) => (
 							<Icon key={i} status={f.status} />
 						))}
 					</div>
-					<button type="button" className="btn btn-primary" onClick={() => downloadFiles() }>
+					<button type="button" className="btn btn-primary" onClick={() => downloadFiles()}>
 						Download files
 					</button>
 				</>
